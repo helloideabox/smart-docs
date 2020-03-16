912871ce7c1c296012cfbe09ae9fef18152aad65
Nirbhay Singh Rao <nirbhay@live.in>
2020-03-16 16:04:42 +0530

    added new option and save notification

--- classes/class-easy-doc-loader.php
+++ classes/class-easy-doc-loader.php
@@ -49,6 +49,10 @@ class  Easy_Doc_Loader {
 
 		// Action to register setting for get_option function.
 		add_action( 'init', array( $this, 'register_plugin_settings' ) );
+
+		// To load search results from ajax request.
+		add_action( 'wp_ajax_ed_load_search_results', array( $this, 'ed_load_search_results' ) );
+		add_action( 'wp_ajax_nopriv_ed_load_search_results', array( $this, 'ed_load_search_results' ) );
 	}
 
 	/**
@@ -76,7 +80,65 @@ class  Easy_Doc_Loader {
 		}
 	}
 
+	/**
+	 * For render the search result.
+	 */
+	public function ed_load_search_results() {
+		// Checking for correct ajax request.
+		if ( check_ajax_referer( 'docs_search', 'security' ) ) {
+			if ( isset( $_POST['query'] ) && ! empty( $_POST['query'] ) ) {
+				$query = sanitize_text_field( wp_unslash( $_POST['query'] ) );
+			} else {
+				$query = esc_html__( 'No docs found', 'easydoc' );
+			}
+		} else {
+			esc_attr_e( 'Nonce is invalid', 'easydoc' );
+		}
 
+		// To show which post to show.
+		$selected_post_types = get_option( 'ed_post_type_selected' );
+		$selected_post_types = ! $selected_post_types ? array( 'post', 'page' ) : $selected_post_types;
+
+		// WordPress Query arguments.
+		$query_args = array(
+			'post_type'   => $selected_post_types,
+			'post_status' => 'publish',
+			's'           => $query,
+		);
+
+		$search_results = new WP_Query( $query_args );
+
+		ob_start();
+		?>
+
+		<ul class="ed-search-result">
+
+			<?php
+			if ( $search_results->have_posts() ) :
+				while ( $search_results->have_posts() ) :
+					$search_results->the_post();
+					?>
+					<li class="ed-search-list">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</li>
+					<?php
+				endwhile;
+			else :
+				esc_attr_e( 'No Doc was found', 'easydoc' );
+			endif;
+			?>
+
+		</ul>
+		<?php
+
+		wp_reset_postdata();
+		$content = ob_get_clean();
+
+		echo $content;
+
+		// To remove 0 appending at the end of the response.
+		wp_die();
+	}
 
 	/**
 	 * Function to enque admin side script(Settings page).
@@ -317,6 +379,16 @@ class  Easy_Doc_Loader {
 		register_setting(
 			'easy-doc-settings-group',
 			'ed_enable_live_search',
+			array(
+				'type'         => 'boolean',
+				'show_in_rest' => true,
+				'default'      => true,
+			)
+		);
+
+		register_setting(
+			'easy-doc-settings-group',
+			'ed_show_last_update_time',
 			array(
 				'type'         => 'boolean',
 				'show_in_rest' => true,
--- templates/easy-doc-shortcode.php
+++ templates/easy-doc-shortcode.php
@@ -14,12 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 // To render the search box of WordPress.
 add_shortcode( 'easy_doc_wp_live_search', 'easy_doc_render_search_box' );
 
-// To load search results from ajax request.
-add_action( 'wp_ajax_ed_load_search_results', 'ed_load_search_results' );
-add_action( 'wp_ajax_nopriv_ed_load_search_results', 'ed_load_search_results' );
-
-
-
 /**
  * For rendering the search box.
  *
@@ -43,66 +37,5 @@ function easy_doc_render_search_box( $atts, $content = null ) {
 	<?php
 	// Returning the html document.
 	return ob_get_clean();
-}
-
-
-/**
- * For render the search result.
- */
-function ed_load_search_results() {
-	// Checking for correct ajax request.
-	if ( check_ajax_referer( 'docs_search', 'security' ) ) {
-		if ( isset( $_POST['query'] ) && ! empty( $_POST['query'] ) ) {
-			$query = sanitize_text_field( wp_unslash( $_POST['query'] ) );
-		} else {
-			$query = esc_html__( 'No docs found', 'easydoc' );
-		}
-	} else {
-		esc_attr_e( 'Nonce is invalid', 'easydoc' );
-	}
-
-	// To show which post to show.
-	$selected_post_types = get_option( 'ed_post_type_selected' );
-	$selected_post_types = ! $selected_post_types ? array( 'post', 'page' ) : $selected_post_types;
-
-	// WordPress Query arguments.
-	$query_args = array(
-		'post_type'   => $selected_post_types,
-		'post_status' => 'publish',
-		's'           => $query,
-	);
-
-	$search_results = new WP_Query( $query_args );
-
-	ob_start();
-	?>
-
-	<ul class="ed-search-result">
-
-		<?php
-		if ( $search_results->have_posts() ) :
-			while ( $search_results->have_posts() ) :
-				$search_results->the_post();
-				?>
-				<li class="ed-search-list">
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				</li>
-				<?php
-			endwhile;
-		else :
-			esc_attr_e( 'No Doc was found', 'easydoc' );
-		endif;
-		?>
-
-	</ul>
-	<?php
-
-	wp_reset_postdata();
-	$content = ob_get_clean();
-
-	echo $content;
-
-	// To remove 0 appending at the end of the response.
-	wp_die();
 }
 
