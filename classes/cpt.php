d06276829e313c224153bd0e19378ac6f7a2b38c
Nirbhay Singh <nirbhay@live.in>
2020-10-15 16:37:15 +0530

    Added slug rewrite code and formatted file using phpcbf.

--- classes/cpt.php
+++ classes/cpt.php
@@ -3,7 +3,7 @@ namespace SmartDocs;
 
 /**
  * Cpt Manager Class.
- * 
+ *
  * Responsible for creating Custom Post Type.
  *
  * @since 1.0.0
@@ -25,6 +25,8 @@ class Cpt {
 	}
 
 	public function register_cpt_doc_type() {
+
+		// Post Type Name
 
 		// Registering Custom post type(SmartDocs).
 		$labels = array(
@@ -62,9 +64,18 @@ class Cpt {
 			'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'author', 'revisions', 'custom-fields' ),
 		);
 
+		$post_slug = get_option( 'sd_archive_page_slug' );
+
+		if ( empty( $post_slug ) ) {
+			$post_slug = 'smart-docs';
+		}
+
+		$args['rewrite'] = apply_filters( 'smartdocs_cpt_rewrite_slug', array( 'slug' => $post_slug ) );
+
 		register_post_type( $this->cpt_name, $args );
 
-		// Resgistering Custom categories taxonomy.
+		// Resgistering Custom Category's taxonomy.
+
 		$category_labels = array(
 			'name'              => esc_html__( 'Docs Categories', 'smart-docs' ),
 			'singular_name'     => esc_html__( 'Doc Category', 'smart-docs' ),
@@ -87,6 +98,14 @@ class Cpt {
 			'show_admin_column' => true,
 			'show_in_rest'      => true,
 		);
+
+		$category_slug = get_option( 'sd_category_slug' );
+
+		if ( empty( $category_slug ) ) {
+			$category_slug = 'smartdocs_category';
+		}
+
+		$category_args['rewrite'] = apply_filters( 'smartdocs_category_rewrite_slug', array( 'slug' => $category_slug ) );
 
 		register_taxonomy( 'smartdocs_category', $this->cpt_name, $category_args );
 
@@ -118,6 +137,16 @@ class Cpt {
 			'show_in_rest'      => true,
 		);
 
+		$tag_slug = get_option( 'sd_tag_slug' );
+
+		if ( empty( $tag_slug ) ) {
+			$tag_slug = 'smartdocs_tag';
+		}
+
+		$tag_args['rewrite'] = apply_filters( 'smartdocs_tag_rewrite_slug', array( 'slug' => $tag_slug ) );
+
 		register_taxonomy( 'smartdocs_tag', $this->cpt_name, $tag_args );
+
+		flush_rewrite_rules();
 	}
 }
