71033d0621c13a2cb7826ff176f188a97499e37e
Nirbhay Singh <nirbhay@live.in>
2020-11-19 16:16:28 +0530

    Added do_action smartdocs_archive_content to render the categories list.

--- templates/smart-docs-archive-template.php
+++ templates/smart-docs-archive-template.php
@@ -53,56 +53,23 @@ do_action( 'smartdocs_before_main_content' );
 
 // Post condition and loop for displaying post.
 if ( have_posts() ) {
-	$args = array(
-		'hide_empty' => apply_filters( 'smartdocs_archive_hide_empty_categories', false ),
-	);
 
 	/**
-	 * Set layout mode
+	 * Hook: smartdocs_archive_content.
+	 *
+	 * @hooked smartdocs_archive_content - 10
 	 */
-	$layout = get_theme_mod( 'smartdocs_archive_layout_setting' );
+	do_action( 'smartdocs_archive_content' );
 
-	if ( empty( $layout ) ) {
-		$layout = 'list';
-	}
-
-	$layoutFunction = 'smartdocs_categories_' . $layout . '_layout';
-
-	$terms = get_terms( 'smartdocs_category', $args );
-	?>
-
-		<?php if ( $terms ) : ?>
-
-		<div class="sd-archive-categories-wrap">
-			<div class="smartdocs-inner smartdocs-cat-<?php echo esc_html( $layout ); ?>-layout">
-				<div class="smartdocs-archive-categories">
-			<?php
-			// Looping through all the terms.
-			foreach ( $terms as $t ) :
-				// Checking if they have parent or not.
-				if ( 0 === $t->parent ) :
-					?>
-						<?php $layoutFunction( $t ); ?>
-
-					<?php
-				endif;
-			endforeach;
-			?>
-				</div>
-			</div>
-		</div>
-		<?php endif ?> 
-
-	<?php
 } else {
 	esc_html_e( 'Not yet started. Add some categories to see them on the SmartDocs Archive Page. ', 'smart-docs' );
 }
 
-/**
- * Hook - smartdocs_after_main_content.
- *
- * @hooked smartdocs_output_content_wrapper_end - 10
- */
-do_action( 'smartdocs_after_main_content' );
+	/**
+	 * Hook - smartdocs_after_main_content.
+	 *
+	 * @hooked smartdocs_output_content_wrapper_end - 10
+	 */
+	do_action( 'smartdocs_after_main_content' );
 
-get_footer();
+	get_footer();
