5c870a35bfc6b19ad92d9481d5e49d74bc5d64a0
Nirbhay Singh <nirbhay@live.in>
2020-11-19 16:14:03 +0530

    Renamed sd- to smartdocs- and removed not required functions.

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -38,7 +38,7 @@ function smartdocs_search_form() {
 
 function smartdocs_output_content_wrapper() {
 	?>
-	<main class="sd-wrap">
+	<main class="smartdocs-wrap">
 	<?php
 }
 
@@ -57,53 +57,8 @@ function smartdocs_output_page_wrap( $content ) {
 	return $content;
 }
 
-function smartdocs_categories_grid_layout( $term ) {
-	?>
-	<div class="smartdocs-category layout-grid">
-		<a href="<?php echo esc_html( get_term_link( $term ) ); ?>" class="sd-sub-archive-categories-post">
-			<h4 class="sd-archive-cat-title">
-				<?php echo esc_html( $term->name ); ?>
-			</h4>
-			<p class="sd-archive-post-count">
-				<?php
-				// Checking if the Article is greter than 0 or 1.
-				if ( 0 === $term->count ) {
-					echo esc_html( $term->count ) . ' Article';
-				} else {
-					/* translators: %s: search term */
-					$article = sprintf( _n( '%d Article', '%d Articles', wp_get_postcount( $term->term_id, $term->taxonomy ), 'smart-docs' ), number_format_i18n( wp_get_postcount( $term->term_id, $term->taxonomy ) ) );
-					echo esc_html( $article );
-				}
-				?>
-			</p>
-		</a>
-	</div>
-	<?php
-}
+function smartdocs_archive_content() {
 
-function smartdocs_categories_list_layout( $term ) {
-	?>
-	<div class="smartdocs-category layout-list">
-		<div class="category-title">
-			<a href="<?php echo esc_html( get_term_link( $term ) ); ?>" class="sd-sub-archive-categories-post">
-				<h4 class="sd-archive-cat-title">
-					<?php echo esc_html( $term->name ); ?>
-				</h4>
-			</a>
-		</div>
-		<div class="category-count">
-			<p class="sd-archive-post-count">
-				<?php
-				// Checking if the Article is greter than 0 or 1.
-				if ( 0 === $term->count ) {
-					echo esc_html( $term->count ) . ' Article';
-				} else {
-					/* translators: %s: search term */
-					$article = sprintf( _n( '%d Article', '%d Articles', wp_get_postcount( $term->term_id, $term->taxonomy ), 'smart-docs' ), number_format_i18n( wp_get_postcount( $term->term_id, $term->taxonomy ) ) );
-					echo esc_html( $article );
-				}
-				?>
-			</p>
-		</div>
-	</div>
-<?php } ?>
+	echo smartdocs_render_categories( $args );
+
+}
