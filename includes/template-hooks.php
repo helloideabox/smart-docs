c4dbce780aa0b2ea8b58a0d8d70583dab82931f5
Nirbhay Singh <nirbhay@live.in>
2020-12-02 15:20:26 +0530

    Added Single Doc Page Hooks.

--- includes/template-hooks.php
+++ includes/template-hooks.php
@@ -8,6 +8,9 @@
  * @package SmartDocs
  */
 
+ /**
+  * Hooked on Category Archive Page.
+  */
 add_action( 'smartdocs_archive_description', 'smartdocs_archive_description', 10 );
 
 add_action( 'smartdocs_archive_before_header_end', 'smartdocs_search_form', 10 );
@@ -19,3 +22,9 @@ add_action( 'smartdocs_after_main_content', 'smartdocs_output_content_wrapper_en
 add_filter( 'the_content', 'smartdocs_output_page_wrap' );
 
 add_action( 'smartdocs_archive_content', 'smartdocs_archive_content', 10);
+
+/**
+ * Hooked on Single Doc Page
+ */
+add_action( 'smartdocs_before_main_content_single_doc', 'smartdocs_output_content_wrapper', 10);
+add_action( 'smartdocs_after_main_content_single_doc', 'smartdocs_output_content_wrapper_end', 10);
