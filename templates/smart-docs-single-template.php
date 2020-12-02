de220771ac1674b49e6d7099b5c23e7bf5eda4d4
Nirbhay Singh <nirbhay@live.in>
2020-12-02 15:24:44 +0530

    Updated Single Page Template.

--- templates/smart-docs-single-template.php
+++ templates/smart-docs-single-template.php
@@ -9,52 +9,56 @@
 
 get_header();
 
-// display live search box.
-echo do_shortcode( '[smart_doc_wp_live_search]' );
+get_style_depends('smartdocs-single-doc', 'single-doc');
+
+/**
+ * Hook: smartdocs_before_main_content_single_post
+ */
+do_action( 'smartdocs_before_main_content_single_doc' );
 
 if ( have_posts() ) :
 	?>
 
-<div class="smartdocs-wrap smartdocs-single-post-wrap">
-	<div class="smartdocs-single-post-container">
-		<?php while ( have_posts() ): the_post(); ?>
+<div class="smartdocs-single-doc-wrap">
+	<div class="smartdocs-single-doc-content-wrap">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>	
+				<header class="smartdocs-single-doc-header">
+					<h1 class="smartdocs-single-doc-title"><?php the_title(); ?></h1>
+					<?php
+					if ( function_exists( 'yoast_breadcrumb' ) ) {
+						yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+					}
+					?>
+				</header>
 
-		<div class="smartdocs-single-post">		
-			<div class="smartdocs-single-post-header">
-				<h1 class="smartdocs-title"><?php the_title(); ?></h1>
+				<article class="smartdocs-single-doc-content">
+					<?php the_post_thumbnail(); ?>
+					<?php the_content(); ?>
+					<?php
+					// If post last updated time is on.
+					$is_last_updatsd_time_on = get_option( 'sd_show_last_update_time' );
+					if ( '1' === $is_last_updatsd_time_on ) {
+						?>
+
+						<div class="smartdocs-single-post-last-update-time"><?php esc_attr_e( 'Updated on ' . get_the_date( 'F j, Y' ), 'smart-docs' ); ?></div>
+
+					<?php } ?>
+					<?php
+					// To get the related tags of that post.
+					the_terms( get_the_ID(), 'smartdocs_tag', '<ul class="smart-docs-tag"><span class="smart-docs-tag-label">Tagged Under: </span><li>', ',</li><li>', '</li></ul>' );
+					?>
+				</article>
+
 				<?php
-				if ( function_exists( 'yoast_breadcrumb' ) ) {
-					yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+				// If comments are open or we have at least one comment, load up the comment template.
+				$is_comment_template_on = get_option( 'sd_turnoff_doc_comment' );
+				if ( ! ( '1' === $is_comment_template_on ) ) {
+					comments_template();
 				}
 				?>
-			</div>
-
-			<div class="smartdocs-single-post-content">
-				<?php the_post_thumbnail(); ?>
-				<?php the_content(); ?>
-				<?php
-				// If post last updated time is on.
-				$is_last_updatsd_time_on = get_option( 'sd_show_last_update_time' );
-				if ( '1' === $is_last_updatsd_time_on ) {
-					?>
-
-					<div class="smartdocs-single-post-last-update-time"><?php esc_attr_e( 'Updated on ' . get_the_date( 'F j, Y' ), 'smart-docs' ); ?></div>
-
-				<?php } ?>
-				<?php
-				// To get the related tags of that post.
-				the_terms( get_the_ID(), 'smartdocs_tag', '<ul class="smart-docs-tag"><span class="smart-docs-tag-label">Tagged Under: </span><li>', ',</li><li>', '</li></ul>' );
-				?>
-			</div>
-
-			<?php
-			// If comments are open or we have at least one comment, load up the comment template.
-			$is_comment_template_on = get_option( 'sd_turnoff_doc_comment' );
-			if ( ! ( '1' === $is_comment_template_on ) ) {
-				comments_template();
-			}
-			?>
-		</div>
 		<?php endwhile; ?>
 	</div>
 
@@ -76,4 +80,15 @@ if ( have_posts() ) :
 endif;
 ?>
 
-<?php get_footer(); ?>
+
+
+<?php
+
+/**
+ * Hook: smartdocs_after_main_content_single_post
+ */
+do_action( 'smartdocs_after_main_content_single_doc' );
+
+	get_footer();
+
+?>
