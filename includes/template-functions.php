6fcf1f6394eaa2c8a39fce77bec003fdcb1db8a3
Nirbhay Singh <nirbhay@live.in>
2021-02-03 11:41:14 +0530

    Updated HTML to keep collapsible toc opened by default.

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -207,12 +207,12 @@ if ( ! function_exists( 'smartdocs_render_toc' ) ) {
 			<div class="smartdocs-toc-title">
 				<span><?php esc_html_e( $title, 'smart-docs' ); ?></span>
 				<?php if ( 'yes' === get_theme_mod( 'smartdocs_toc_collapsible' ) ) :?>
-				<span class="smartdocs-toc-open toc-opened">
+				<span class="smartdocs-toc-open">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
 						<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
 					</svg>
 				</span>
-				<span class="smartdocs-toc-close">
+				<span class="smartdocs-toc-close toc-closed">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
 						<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
 					</svg>
